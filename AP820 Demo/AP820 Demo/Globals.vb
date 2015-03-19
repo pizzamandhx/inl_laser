31afea35ae1ff4abdda8a474ab194e8686b188ab
pizzamandhx <mr_ocean719@yahoo.com>
2015-03-19 10:34:15 -0600

    make clean
    deleted all

--- AP820 Demo/AP820 Demo/Globals.vb
+++
@@ -1,8 +0,0 @@
-﻿Module Globals
-    Public AP820Laser1 As New AP820Class
-    Public AP820Laser2 As New AP820Class
-    Public frmSecondLaser As New SecondLaserForm
-    Public Filename As String
-    Public ValidFileNameFlag As Boolean
-    Public SendToFileFlag As Boolean
-End Module
